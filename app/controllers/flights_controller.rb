18e47fff5a6553241360c10a396d3205c24e1d06
burvxs <benny.purvis@hotmail.com>
2020-08-25 18:08:12 +1000

    Added respond to

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -8,7 +8,7 @@ class FlightsController < ApplicationController
   def index 
     respond_to do |format|
       format.html
-      format.json { render :json Flight.all }
+      format.json { render :json => Flight.all }
     end
   end
 
