072faf363cb157c52c09c527863963f9199a12f5
benl798 <ben.l7@hotmail.com>
2020-08-25 17:27:44 +1000

    CRUD for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,35 @@
 class UsersController < ApplicationController
+
+# CREATE ###########################
   def new
+    @user = User.new
   end
 
   def create
   end
 
+  # READ #############################
   def index
+    @users = User.all
   end
 
   def show
   end
 
+# UPDATE #############################
   def edit
   end
 
   def update
   end
 
+# DESTROY #############################
   def destroy
   end
+
+  private
+
+    def user_params
+      params.require( :user ).permit( :name, :admin )
+    end
 end
